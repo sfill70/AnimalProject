a041d52500e2f1c23f0e9334d2994e4cbe165d6d
sfill70 <sergfill70@yandex.ru>
2022-08-14 14:09:01 +0300

    change animal static initialization

--- src/main/java/com/example/animalproject/app/land/UtilAnimal.java
+++ src/main/java/com/example/animalproject/app/land/UtilAnimal.java
@@ -5,12 +5,22 @@ import com.example.animalproject.app.land.resident.herbivore.*;
 import com.example.animalproject.app.land.resident.predator.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
 
 public class UtilAnimal {
-    private static String root = System.getProperty("user.dir");
-    private static final String CLAZZ_PATH = String.join(File.separator, root, "src",
-            "main","java","com","example","animalproject","app","land","resident");
+    private static final String root = System.getProperty("user.dir");
+    private static final String CLAZZ_PATH = String.join(File.separator, root, /*"AnimalProject",*/ "src",
+            "main", "java", "com", "example", "animalproject", "app", "land", "resident");
+    private static final String animalPath = String.join(File.separator, root, /*"AnimalProject",*/ "src",
+            "main", "java", "com", "example", "animalproject", "app", "land", "resident", "Animal.java");
     private static final ConcurrentHashMap<Class<?>, Integer> mapAmountAnimal = new ConcurrentHashMap<>();
     private static final ConcurrentHashMap<Class<?>, Integer> mapSpeedAnimal = new ConcurrentHashMap<>();
     private static final ConcurrentHashMap<Class<?>, Integer> abilityToReproduce = new ConcurrentHashMap<>();
@@ -19,21 +29,19 @@ public class UtilAnimal {
      * Нужен для заполнения Map из статики животных
      */
     static {
-        new Wolf();
-        new Fox();
-        new Eagle();
-        new Boa();
-        new Bear();
-        new Sheep();
-        new Rabbit();
-        new Mouse();
-        new Horse();
-        new Goat();
-        new Duck();
-        new Deer();
-        new Caterpillar();
-        new Buffalo();
-        new Boar();
+        try {
+            animalsInitialization();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void putMapAmountAnimal(Class<?> clazz, int amount) {
@@ -64,5 +72,35 @@ public class UtilAnimal {
         return amount < mapAmountAnimal.get(animal.getClass());
     }
 
+    /**
+     * Создает по одному экземнляру животного для инициализации из статики
+     */
+    private static void animalsInitialization() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        List<String> listClazzName = getlistClazzName();
+        String baseClazzName = Animal.class.getName().replace(".Animal", "");
+        for (String clazzName : listClazzName
+        ) {
+            initializationAnimal(baseClazzName + clazzName);
+        }
+    }
 
+    private static List<String> getlistClazzName() {
+        List<String> result = null;
+        try (Stream<Path> walk = Files.walk(Paths.get(CLAZZ_PATH))) {
+            result = walk.filter(Files::isRegularFile).filter(path -> !path.endsWith("Animal.java")).
+                    filter(path -> !path.endsWith("Herbivore.java")).filter(path -> !path.endsWith("Predator.java"))
+                    .map(path -> Paths.get(animalPath).relativize(path)).map(x -> x.toString()
+                            .replace(".java", "").replace(".", "")
+                            .replace(File.separator, ".")).toList();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private static void initializationAnimal(String clazzName) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException, ClassNotFoundException {
+        Class<?> clazz = Class.forName(clazzName);
+        Constructor<?> animalConstructor = clazz.getConstructor();
+        Animal animalAny = (Animal) animalConstructor.newInstance();
+    }
 }
