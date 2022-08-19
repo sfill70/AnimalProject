b91092d818c534377c72d55a51906b479a24ba4a
sfill70 <sergfill70@yandex.ru>
2022-08-19 10:18:24 +0300

    edit Chupacabra

--- src/main/java/com/example/animalproject/app/land/resident/predator/Chupacabra.java
+++ src/main/java/com/example/animalproject/app/land/resident/predator/Chupacabra.java
@@ -3,18 +3,13 @@ package com.example.animalproject.app.land.resident.predator;
 import com.example.animalproject.app.land.UtilAnimal;
 import com.example.animalproject.app.land.resident.herbivore.*;
 
-public class Chupacabra extends Wolf{
+public class Chupacabra extends Wolf {
     static {
         UtilAnimal.putMapAmountAnimal(Chupacabra.class, 30);
-        Wolf.chanceSuccessfulHunt.put(Horse.class, 30);
-        Wolf.chanceSuccessfulHunt.put(Deer.class, 35);
-        Wolf.chanceSuccessfulHunt.put(Rabbit.class, 50);
-        Wolf.chanceSuccessfulHunt.put(Mouse.class, 50);
-        Wolf.chanceSuccessfulHunt.put(Goat.class, 50);
-        Wolf.chanceSuccessfulHunt.put(Sheep.class, 50);
         UtilAnimal.putSpeedAnimal(Chupacabra.class, 3);
         UtilAnimal.putAbilityToReproduce(Chupacabra.class, 4);
     }
+
     public Chupacabra() {
         super();
         this.foodConsumption = 16;
